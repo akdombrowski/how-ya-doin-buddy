225cd4b8b949f39e36d176776576cded2ecbdf1c
Anthony Dombrowski <akdflyinvee73@gmail.com>
2022-12-31 07:02:13 -0600

    add head params

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
     <Html>
-      <Head>
+      <Head params={{
+        slug: "slug"
+      }}>
 
       </Head>
       <body>
