fba1c1619d38fe01ef6f6b9e893c63fbb2bd3297
Anthony Dombrowski <akdflyinvee73@gmail.com>
2022-12-31 07:19:52 -0600

    remove head params for root head and layout

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 // import React from "react";
 import DefaultTags from "./DefaultTags";
 
-export default function Head({ params }: { params: { slug: string } }) {
+export default function Head() {
   return (
     <>
       <title>My Page</title>
