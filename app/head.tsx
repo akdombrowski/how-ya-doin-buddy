9fb352a9894b7659a02dcf02fe5a4f84c4468d67
Anthony Dombrowski <akdflyinvee73@gmail.com>
2022-12-31 07:06:50 -0600

    default slug

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import DefaultTags from "./DefaultTags";
 
-export default function Head({ params }: { params: { slug: string } }) {
+export default function Head({
+  params = { slug: "" },
+}: {
+  params: { slug: string };
+}) {
   return (
     <>
       <title>My Page</title>
