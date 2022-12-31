cd495df350bc86c72a04cd69f96561321d049259
Anthony Dombrowski <akdflyinvee73@gmail.com>
2022-12-31 08:09:56 -0600

    migrating from _app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,20 @@ import { ColorModeScript, theme } from "@chakra-ui/react";
 import { Html, Main, NextScript } from "next/document";
 import Head from "./head";
 
+import { ChakraProvider } from "@chakra-ui/react";
+
+// 1. Import the extendTheme function
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -14,6 +28,8 @@ export default function RootLayout({
         {/* Color Mode Script */}
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <NextScript />
+
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
       </body>
     </Html>
   );
