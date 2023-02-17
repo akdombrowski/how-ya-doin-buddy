d495e3a030d31ff86152f7d5194e7bc869777f65
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-02-16 18:25:17 -0600

    remove stack component

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,147 +23,143 @@ const IndexPage = () => {
   if (session) {
     return (
       <Container maxWidth="xs">
-        <Stack spacing={2}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            Signed in as {session.user.email} <br />
+          </Grid>
+        </Grid>
+
+        <form id="form" onSubmit={handleSubmit(onSubmit)}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              Signed in as {session.user.email} <br />
+              <label>How ya doin, buddy?</label>
+              <Controller
+                name="mood"
+                control={control}
+                defaultValue={5}
+                render={({ field }) => (
+                  <Slider
+                    {...field}
+                    onChange={(_, value) => {
+                      field.onChange(value);
+                    }}
+                    valueLabelDisplay="auto"
+                    marks
+                    max={10}
+                    min={1}
+                    step={1}
+                  />
+                )}
+              />
             </Grid>
           </Grid>
 
-          <Stack spacing={2}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <label>How ya doin, buddy?</label>
-                  <Controller
-                    name="mood"
-                    control={control}
-                    defaultValue={5}
-                    render={({ field }) => (
-                      <Slider
-                        {...field}
-                        onChange={(_, value) => {
-                          field.onChange(value);
-                        }}
-                        valueLabelDisplay="auto"
-                        marks
-                        max={10}
-                        min={1}
-                        step={1}
-                      />
-                    )}
-                  />
-                </Grid>
-              </Grid>
-
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <label>Are ya gettin shit done?</label>
-                  <Controller
-                    name="motivation"
-                    control={control}
-                    defaultValue={5}
-                    render={({ field }) => (
-                      <Slider
-                        {...field}
-                        onChange={(_, value) => {
-                          field.onChange(value);
-                        }}
-                        valueLabelDisplay="auto"
-                        marks
-                        max={10}
-                        min={1}
-                        step={1}
-                      />
-                    )}
-                  />
-                </Grid>
-              </Grid>
-
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <label>Feelin like a social butterfly?</label>
-                  <Controller
-                    name="social"
-                    control={control}
-                    defaultValue={5}
-                    render={({ field }) => (
-                      <Slider
-                        {...field}
-                        onChange={(_, value) => {
-                          field.onChange(value);
-                        }}
-                        valueLabelDisplay="auto"
-                        marks
-                        max={10}
-                        min={1}
-                        step={1}
-                      />
-                    )}
-                  />
-                </Grid>
-              </Grid>
-
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <label>People pissing you off?</label>
-                  <Controller
-                    name="anger"
-                    control={control}
-                    defaultValue={5}
-                    render={({ field }) => (
-                      <Slider
-                        {...field}
-                        onChange={(_, value) => {
-                          field.onChange(value);
-                        }}
-                        valueLabelDisplay="auto"
-                        marks
-                        max={10}
-                        min={1}
-                        step={1}
-                      />
-                    )}
-                  />
-                </Grid>
-              </Grid>
-
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <label>U pissin other people off?</label>
-                  <Controller
-                    name="annoying"
-                    control={control}
-                    defaultValue={5}
-                    render={({ field }) => (
-                      <Slider
-                        {...field}
-                        onChange={(_, value) => {
-                          field.onChange(value);
-                        }}
-                        valueLabelDisplay="auto"
-                        marks
-                        max={10}
-                        min={1}
-                        step={1}
-                      />
-                    )}
-                  />
-                </Grid>
-              </Grid>
-
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <input type="submit" />
-                </Grid>
-              </Grid>
-            </form>
-
+          <Grid container spacing={2}>
             <Grid item xs={12}>
-              <button onClick={() => signOut()}>Sign out</button>
+              <label>Are ya gettin shit done?</label>
+              <Controller
+                name="motivation"
+                control={control}
+                defaultValue={5}
+                render={({ field }) => (
+                  <Slider
+                    {...field}
+                    onChange={(_, value) => {
+                      field.onChange(value);
+                    }}
+                    valueLabelDisplay="auto"
+                    marks
+                    max={10}
+                    min={1}
+                    step={1}
+                  />
+                )}
+              />
             </Grid>
-          </Stack>
-        </Stack>
+          </Grid>
+
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <label>Feelin like a social butterfly?</label>
+              <Controller
+                name="social"
+                control={control}
+                defaultValue={5}
+                render={({ field }) => (
+                  <Slider
+                    {...field}
+                    onChange={(_, value) => {
+                      field.onChange(value);
+                    }}
+                    valueLabelDisplay="auto"
+                    marks
+                    max={10}
+                    min={1}
+                    step={1}
+                  />
+                )}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <label>People pissing you off?</label>
+              <Controller
+                name="anger"
+                control={control}
+                defaultValue={5}
+                render={({ field }) => (
+                  <Slider
+                    {...field}
+                    onChange={(_, value) => {
+                      field.onChange(value);
+                    }}
+                    valueLabelDisplay="auto"
+                    marks
+                    max={10}
+                    min={1}
+                    step={1}
+                  />
+                )}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <label>U pissin other people off?</label>
+              <Controller
+                name="annoying"
+                control={control}
+                defaultValue={5}
+                render={({ field }) => (
+                  <Slider
+                    {...field}
+                    onChange={(_, value) => {
+                      field.onChange(value);
+                    }}
+                    valueLabelDisplay="auto"
+                    marks
+                    max={10}
+                    min={1}
+                    step={1}
+                  />
+                )}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <input type="submit" />
+            </Grid>
+          </Grid>
+        </form>
+
+        <Grid item xs={12}>
+          <button onClick={() => signOut()}>Sign out</button>
+        </Grid>
       </Container>
     );
   }
