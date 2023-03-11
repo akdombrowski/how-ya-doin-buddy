f41b98cda6b92c51d7b5c925d1f30bb97951b082
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-03-10 23:46:24 -0600

    formatted sign on screen

--- src/components/HomeNotSignedIn.tsx
+++ src/components/HomeNotSignedIn.tsx
@@ -1,6 +1,7 @@
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 import { ThemeOptions } from "@mui/material";
 import { signIn, useSession } from "next-auth/react";
 
@@ -26,11 +27,34 @@ const HomeNotSignedIn = () => {
   const { data: session } = useSession();
 
   return (
-    <Container>
-      <Typography>Not Signed In</Typography>
-      <Button variant="contained" onClick={() => signIn()}>
-        Sign In
-      </Button>
+    <Container maxWidth="xl" sx={{ height: "100vh" }}>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        alignContent="center"
+        sx={{ height: "100%", justifyContent: "center" }}
+        spacing={0}
+      >
+        <Grid item xs={6}>
+          <Typography variant="h1" sx={{ textAlign: "center" }}>
+            how ya doin, buddy?
+          </Typography>
+        </Grid>
+
+        <Grid item xs={3}>
+          <Button
+            variant="contained"
+            size="large"
+            sx={{
+              width: "100%",
+            }}
+            onClick={() => signIn()}
+          >
+            Sign In
+          </Button>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
